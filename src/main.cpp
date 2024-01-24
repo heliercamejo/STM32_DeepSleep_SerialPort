ca0111bd4b1650e506b8e05f18a6c471f79523be
HelierDCA <hdcamejo00@gmail.com>
2024-01-24 00:33:54 -0500

    DeepSleep Serial Port interruption wake up

--- src/main.cpp
+++ src/main.cpp
@@ -7,33 +7,60 @@
 #include "stm32f4xx_hal_usart.h"
 #include "stm32f4xx_hal_uart.h"
 
-HardwareSerial Serial2(PA3,PA2);
+//HardwareSerial Serial2(PA3,PA2);
 
 void repetitionsIncrease(void);
 
 bool sleep = true;
 volatile int repetitions = 0;
+String inputString = "";
 
 void setup() {
-
-  pinMode(PC13, OUTPUT);
+  
+  SerialUSB.begin(115200);
+  Serial2.begin(115200);
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
   
   LowPower.begin();
-  LowPower.attachInterruptWakeup(PA3, repetitionsIncrease, RISING, SLEEP_MODE);
+  LowPower.attachInterruptWakeup(PA3, repetitionsIncrease, RISING, DEEP_SLEEP_MODE);
    
 }
 
 void loop() {
-  for (int i = 0; i < repetitions; i++) {
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(500);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(500);
-  }
-  LowPower.sleep();
 
+  if(sleep){
+    sleep = false;
+    SerialUSB.println("Falling sleep");
+    digitalWrite(LED_BUILTIN, HIGH);
+    
+    attachInterrupt(PA3, repetitionsIncrease, RISING);
+    
+    LowPower.deepSleep();
+
+    SerialUSB.println("Waking up"); 
+    digitalWrite(LED_BUILTIN, LOW); 
+  }
+
+  if(Serial2.available()){
+    inputString = Serial2.readString();
+    int lengthString = inputString.length();
+    char arrChar[lengthString];
+    inputString.toCharArray(arrChar,lengthString);
+    
+    Serial2.print(inputString);
+    Serial2.println(repetitions);
+    
+    if(strstr(arrChar,"OK") != 0){
+      sleep = true;
+    }
+  }
 }
 
 void repetitionsIncrease() {
+  
   repetitions ++;
+  detachInterrupt(PA3);
+  Serial2.begin(115200);
 }
